c1198eae46d38b8534f1d665c7d6be41b8e63901
BAMoustapha <bam480836@gmail.com>
2024-04-18 11:17:37 +0000

    ba

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-red-800'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <a className='bg-danger-500'
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
