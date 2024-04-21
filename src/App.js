66904910abafd153c8e8dc810a3c89106e379e14
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-04-21 14:51:30 +0000

    gestion-admin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
+import Footer from './components/footer/footer';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-blue-800'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='bg-danger-500'
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React an node
-        </a>
-      </header>
+      <Footer />
     </div>
   );
 }
