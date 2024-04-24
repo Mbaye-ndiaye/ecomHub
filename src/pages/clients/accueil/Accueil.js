5831535de15ddf20057e2901304a42399effb534
BAMoustapha <bam480836@gmail.com>
2024-04-24 10:00:35 +0000

    ba

--- src/pages/clients/accueil/Accueil.js
+++ src/pages/clients/accueil/Accueil.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Accueil = () => {
   return (
-    <nav className=" p-4">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
+    <nav className="p-4 ">
+      <div className="flex items-center justify-between mx-auto max-w-7xl">
         <div className="flex items-center">
-          <p className="text-white text-xl font-semibold mx-4">Logo</p>
-          <div className='flex mx-auto text-white mx-4'>
+          <p className="mx-4 text-xl font-semibold text-white">Logo</p>
+          <div className='flex mx-4 mx-auto text-white'>
             <p>Home</p>
             <p>Product</p>
             <p>About</p>
@@ -16,7 +16,6 @@ const Accueil = () => {
         <div className="hidden md:block">
           
         </div>
-        {/* <div className="md:hidden">
 
       </div>
     </nav>
